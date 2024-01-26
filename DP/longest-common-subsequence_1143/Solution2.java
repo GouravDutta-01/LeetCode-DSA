0ccb4e04cee0c6691d0e602b7522142e59a172a1
Gourav Dutta <108947265+GouravDutta-01@users.noreply.github.com>
2024-01-26 11:12:41 +0530

    Minor changes

--- DP/longest-common-subsequence_1143/Solution2.java
+++ DP/longest-common-subsequence_1143/Solution2.java
@@ -3,20 +3,19 @@
 //Tabulation Code
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][] = new int[text1.length() + 1][text2.length() + 1];
-        for(int ind1=0; ind1<=text1.length(); ind1++){
-            for(int ind2=0; ind2<=text2.length(); ind2++){
-                if(ind1==0 || ind2==0){
-                    dp[ind1][ind2] = 0;
-                }
-                else if(text1.charAt(ind1-1) == text2.charAt(ind2-1)){
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    dp[ind1][ind2] = Math.max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
             }
         }
-        return dp[text1.length()][text2.length()];  
+        return dp[m][n];     
     }
 }
--- DP/longest-palindromic-subsequence_516/Solution.java
+++ DP/longest-palindromic-subsequence_516/Solution.java
@@ -3,24 +3,23 @@
 //Tabulation Code
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        String revStr = new StringBuilder(s).reverse().toString();
-        return longestCommonSubsequence(s, revStr);  
+        String revS = new StringBuilder(s).reverse().toString();
+        return lcs(s, revS);  
     }
-    public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][] = new int[text1.length() + 1][text2.length() + 1];
-        for(int ind1=0; ind1<=text1.length(); ind1++){
-            for(int ind2=0; ind2<=text2.length(); ind2++){
-                if(ind1==0 || ind2==0){
-                    dp[ind1][ind2] = 0;
-                }
-                else if(text1.charAt(ind1-1) == text2.charAt(ind2-1)){
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+    public int lcs(String a, String b){
+        int m = a.length();
+        int n = b.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(a.charAt(i-1) == b.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    dp[ind1][ind2] = Math.max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
             }
         }
-        return dp[text1.length()][text2.length()];  
+        return dp[m][n];
     }
 }
